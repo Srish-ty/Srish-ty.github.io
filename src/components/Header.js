be3bc1ce9d4dae0a4b755b01d4191b3c70e699ae
Srishty Mangutte <srishtymangutte@gmail.com>
2023-02-05 19:44:01 +0530

    fix: remove unused vars

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 //import styled from "styled-components";
 import { Headi } from './styling/header.style';
 
-
 export default function Header() {
   return (
     <Headi/>
