d5de5574dbd7d3fe9308137aacde0f60291a3b8e
Srishty Mangutte <srishtymangutte@gmail.com>
2023-02-05 19:41:01 +0530

    fix: unused vars

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components';
+//import styled from 'styled-components';
 //import tw from "twin.macro";
 import { NavBar } from './styling/nav.style';
 import { Tab } from './styling/nav.style';
